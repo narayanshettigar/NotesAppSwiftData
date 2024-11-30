127639ceb3ce4fc968c87343ca856f42e59b2a99
narayanshettigar <narayanshettigar16@gmail.com>
2024-11-30 21:18:21 +0530

    initial xcode setup

---
+++ NotesAppSwiftData/NotesAppSwiftData/NotesAppSwiftDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesAppSwiftDataApp.swift
+//  NotesAppSwiftData
+//
+//  Created by Narayan Shettigar on 30/11/24.
+//
+
+import SwiftUI
+
+@main
+struct NotesAppSwiftDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
